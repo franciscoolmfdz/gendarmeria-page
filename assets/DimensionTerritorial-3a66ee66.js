35cd8cda68cb9d1d5f25379d1ea237a9157655eb
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-05-17 23:36:41 -0400

    fix chunks

--- assets/DimensionTerritorial-3a66ee66.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,a as r}from"./CHeaderBar-3afc8f21.js";import{_ as i}from"./ManagementXLS-9924a8d4.js";import{r as _,o as s,d as n,w as o,b as e}from"./index-4805e326.js";const d={__name:"DimensionTerritorial",setup(l){const t=_("Dimensión territorial");return(c,m)=>(s(),n(a,null,{default:o(()=>[e(r,{title:t.value},null,8,["title"]),e(i)]),_:1}))}};export{d as default};
--- assets/ElementosReinsercion-d6b71891.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,a as n}from"./CHeaderBar-3afc8f21.js";import{_}from"./ManagementXLS-9924a8d4.js";import{r as a,o,d as r,w as i,b as e}from"./index-4805e326.js";const d={__name:"ElementosReinsercion",setup(c){const t=a("Elementos de reinserción");return(l,m)=>(o(),r(s,null,{default:i(()=>[e(n,{title:t.value},null,8,["title"]),e(_)]),_:1}))}};export{d as default};
