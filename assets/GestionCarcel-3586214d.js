df335742d70445b33907c6dca8b1242f27a96b0b
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-06-03 11:09:46 -0400

    mejoras para presentacion

--- assets/GestionCarcel-3586214d.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,a}from"./CHeaderBar-bee1a11e.js";import{r as _,o as s,d as l,w as c,b as n}from"./index-76fa7c79.js";const m={__name:"GestionCarcel",setup(r){const e=_("Gestión de la carcel");return(o,i)=>(s(),l(t,null,{default:c(()=>[n(a,{title:e.value},null,8,["title"])]),_:1}))}};export{m as default};
