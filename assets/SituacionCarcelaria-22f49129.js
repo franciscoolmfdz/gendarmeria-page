aa05ff6e1bf9ae653f3ce98e6a66cd299b65a1e1
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-07-05 10:51:10 -0400

    se sube graficos con conexión al api

---
+++ assets/SituacionCarcelaria-22f49129.js
@@ -0,0 +1,1 @@
+import{r as e,o as t,d as _,w as c,_ as r,b as i}from"./index-acf756d2.js";import{_ as s}from"./CHeaderBar-02265e87.js";const m={__name:"SituacionCarcelaria",setup(l){const a=e("Estado de la situación carcelaria");return(n,o)=>(t(),_(r,null,{default:c(()=>[i(s,{title:a.value},null,8,["title"])]),_:1}))}};export{m as default};
--- assets/SituacionCarcelaria-aa0b952b.js
+++
@@ -1,1 +0,0 @@
-import{r as e,o as t,d as _,w as c,_ as r,b as i}from"./index-4bf4a7de.js";import{_ as s}from"./CHeaderBar-922b50aa.js";const m={__name:"SituacionCarcelaria",setup(l){const a=e("Estado de la situación carcelaria");return(n,o)=>(t(),_(r,null,{default:c(()=>[i(s,{title:a.value},null,8,["title"])]),_:1}))}};export{m as default};
