df79125b4fe2ed22a4032d628217b07c28c2e392
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-07-14 21:40:05 -0400

    mejora dimension territorial

---
+++ assets/GestionCarcel-3bad222f.js
@@ -0,0 +1,1 @@
+import{r as t,o as a,e as _,w as s,_ as l,b as c}from"./index-93896bb6.js";import{_ as n}from"./CHeaderBar-28ebd41f.js";const m={__name:"GestionCarcel",setup(r){const e=t("Gestión de la carcel");return(o,i)=>(a(),_(l,null,{default:s(()=>[c(n,{title:e.value},null,8,["title"])]),_:1}))}};export{m as default};
---
+++ assets/SituacionCarcelaria-552da719.js
@@ -0,0 +1,1 @@
+import{r as e,o as t,e as _,w as c,_ as r,b as i}from"./index-93896bb6.js";import{_ as s}from"./CHeaderBar-28ebd41f.js";const m={__name:"SituacionCarcelaria",setup(l){const a=e("Estado de la situación carcelaria");return(n,o)=>(t(),_(r,null,{default:c(()=>[i(s,{title:a.value},null,8,["title"])]),_:1}))}};export{m as default};
