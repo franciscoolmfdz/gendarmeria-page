cb245d4988d21f8be5f52f1c1a0e70fd81bd9d75
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-07-30 14:18:30 -0400

    se agregan graficos y filtro multiple

---
+++ assets/ElementosReinsercion-010656af.js
@@ -0,0 +1,1 @@
+import{r as t,o as n,e as s,w as _,_ as a,b as o}from"./index-986a4269.js";import{_ as r}from"./CHeaderBar-e3b0e061.js";const f={__name:"ElementosReinsercion",setup(l){const e=t("Elementos de reinserción");return(c,i)=>(n(),s(a,null,{default:_(()=>[o(r,{title:e.value},null,8,["title"])]),_:1}))}};export{f as default};
---
+++ assets/SituacionCarcelaria-3278d938.js
@@ -0,0 +1,1 @@
+import{r as e,o as t,e as _,w as c,_ as r,b as i}from"./index-986a4269.js";import{_ as s}from"./CHeaderBar-e3b0e061.js";const m={__name:"SituacionCarcelaria",setup(l){const a=e("Estado de la situación carcelaria");return(n,o)=>(t(),_(r,null,{default:c(()=>[i(s,{title:a.value},null,8,["title"])]),_:1}))}};export{m as default};
