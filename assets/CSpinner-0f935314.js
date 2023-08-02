9d78b18f4e7c2d3fa4da073e3dc19a0911958e83
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-08-02 09:49:38 -0400

    fix detalles despliegue

---
+++ assets/CSpinner-0f935314.js
@@ -0,0 +1,1 @@
+import{o as t,c as n,f as o}from"./index-f56d6358.js";const _={key:0,id:"cover-spin"},s={__name:"CSpinner",props:{activeSpin:{type:Boolean,default:!1}},setup(e){return(a,c)=>e.activeSpin?(t(),n("div",_)):o("",!0)}};export{s as _};
