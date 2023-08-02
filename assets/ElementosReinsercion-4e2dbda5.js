ce394fd2bce78446ce59b324879342413dc45149
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-08-02 10:31:37 -0400

    correciones menores faltantes titulos

---
+++ assets/ElementosReinsercion-4e2dbda5.js
@@ -0,0 +1,1 @@
+import{r as t,o as n,e as s,w as _,_ as a,b as o}from"./index-42b1f99d.js";import{_ as r}from"./CHeaderBar-4c7da320.js";const f={__name:"ElementosReinsercion",setup(l){const e=t("Elementos de reinserción");return(c,i)=>(n(),s(a,null,{default:_(()=>[o(r,{title:e.value},null,8,["title"])]),_:1}))}};export{f as default};
