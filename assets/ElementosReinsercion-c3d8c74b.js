6f7ed85459d2233fb38999aa63e768428ace7a4f
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-08-06 11:23:22 -0400

    fix links y opcion apertura territorial

---
+++ assets/ElementosReinsercion-c3d8c74b.js
@@ -0,0 +1,1 @@
+import{r as t,o as n,e as s,w as _,_ as a,b as o}from"./index-56c60ed9.js";import{_ as r}from"./CHeaderBar-3e34db07.js";const f={__name:"ElementosReinsercion",setup(l){const e=t("Elementos de reinserción");return(c,i)=>(n(),s(a,null,{default:_(()=>[o(r,{title:e.value},null,8,["title"])]),_:1}))}};export{f as default};
