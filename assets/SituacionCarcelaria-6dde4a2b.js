d6d7d91cec31400888f98b239a6e40bf72a920ed
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-08-29 09:03:24 -0400

    cambio api

---
+++ assets/SituacionCarcelaria-6dde4a2b.js
@@ -0,0 +1,1 @@
+import{h as e,o as t,f as _,j as c,_ as i,g as r}from"./index-aa05dffe.js";import{_ as s}from"./CHeaderBar-519cc880.js";const m={__name:"SituacionCarcelaria",setup(l){const a=e("Estado de la situación carcelaria");return(n,o)=>(t(),_(i,null,{default:c(()=>[r(s,{title:a.value},null,8,["title"])]),_:1}))}};export{m as default};
