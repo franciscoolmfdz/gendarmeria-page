a14b9fa3ab76548189c26c552160131c488a5058
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-08-30 00:24:16 -0400

    cambio total

---
+++ assets/SituacionCarcelaria-157ba050.js
@@ -0,0 +1,1 @@
+import{h as e,o as t,f as _,j as c,_ as i,g as r}from"./index-8a5d36ad.js";import{_ as s}from"./CHeaderBar-6b1a30b0.js";const m={__name:"SituacionCarcelaria",setup(l){const a=e("Estado de la situación carcelaria");return(n,o)=>(t(),_(i,null,{default:c(()=>[r(s,{title:a.value},null,8,["title"])]),_:1}))}};export{m as default};
