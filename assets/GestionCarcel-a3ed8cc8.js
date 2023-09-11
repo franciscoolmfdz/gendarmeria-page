b1d12bcc2fd2aab339b6f96cb3f611dfea2d3cec
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-09-11 02:05:36 -0300

    mejoras css estilo nuevo

---
+++ assets/GestionCarcel-a3ed8cc8.js
@@ -0,0 +1,1 @@
+import{h as t,o as a,f as _,j as s,_ as l,g as c}from"./index-56444a41.js";import{_ as n}from"./CHeaderBar-ce0ad327.js";const m={__name:"GestionCarcel",setup(o){const e=t("Gestión de la carcel");return(r,i)=>(a(),_(l,null,{default:s(()=>[c(n,{title:e.value},null,8,["title"])]),_:1}))}};export{m as default};
---
+++ assets/SituacionCarcelaria-54fa2e58.js
@@ -0,0 +1,1 @@
+import{h as e,o as t,f as _,j as c,_ as i,g as r}from"./index-56444a41.js";import{_ as s}from"./CHeaderBar-ce0ad327.js";const m={__name:"SituacionCarcelaria",setup(l){const a=e("Estado de la situación carcelaria");return(n,o)=>(t(),_(i,null,{default:c(()=>[r(s,{title:a.value},null,8,["title"])]),_:1}))}};export{m as default};
