085a06a7631d2d5d0b47c14b7f5ba4e3f24906d8
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-10-01 15:58:10 -0300

    actualizacion datos y modulo creacion usuarios

---
+++ assets/SituacionCarcelaria-44a1bf61.js
@@ -0,0 +1,1 @@
+import{h as e,o as t,f as _,j as c,_ as i,g as r}from"./index-81990198.js";import{_ as s}from"./CHeaderBar-c05e4acb.js";const m={__name:"SituacionCarcelaria",setup(l){const a=e("Estado de la situación carcelaria");return(n,o)=>(t(),_(i,null,{default:c(()=>[r(s,{title:a.value},null,8,["title"])]),_:1}))}};export{m as default};
---
+++ assets/regiones-9e2eaf7c.js
@@ -0,0 +1,1 @@
+import{a as n}from"./api-ec658a74.js";const i="/dmm/regiones",u=async(e,r)=>{if(e!=null&&e.id){let l=`${i}/${e.id}`;return n(l,"GET",null,r)}return n(i,"GET",null,r)};export{u as g};
