027859f1d7ffbabfc942d230514641db6580c34e
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-10-11 11:14:36 -0300

    se agrega fix visual front

---
+++ assets/SituacionCarcelaria-07796757.js
@@ -0,0 +1,1 @@
+import{g as e,o as t,f as _,j as c,_ as i,l}from"./index-1482005a.js";import{_ as r}from"./CHeaderBar-46b15ae3.js";const m={__name:"SituacionCarcelaria",setup(s){const a=e("Estado de la situación carcelaria");return(n,o)=>(t(),_(i,null,{default:c(()=>[l(r,{title:a.value},null,8,["title"])]),_:1}))}};export{m as default};
