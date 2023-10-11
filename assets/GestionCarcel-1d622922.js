f4572ce4b6503934f6c28679e1935d353a116566
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-10-11 10:51:22 -0300

    v1 para revisar

---
+++ assets/GestionCarcel-1d622922.js
@@ -0,0 +1,1 @@
+import{g as t,o as a,f as _,j as s,_ as l,l as c}from"./index-b9c74f25.js";import{_ as n}from"./CHeaderBar-1bcb54ea.js";const m={__name:"GestionCarcel",setup(o){const e=t("Gestión de la carcel");return(r,i)=>(a(),_(l,null,{default:s(()=>[c(n,{title:e.value},null,8,["title"])]),_:1}))}};export{m as default};
---
+++ assets/SituacionCarcelaria-4bb269d9.js
@@ -0,0 +1,1 @@
+import{g as e,o as t,f as _,j as c,_ as i,l}from"./index-b9c74f25.js";import{_ as r}from"./CHeaderBar-1bcb54ea.js";const m={__name:"SituacionCarcelaria",setup(s){const a=e("Estado de la situación carcelaria");return(n,o)=>(t(),_(i,null,{default:c(()=>[l(r,{title:a.value},null,8,["title"])]),_:1}))}};export{m as default};
