a53d2f246b633195f88897f306fc906c223cd1d5
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-10-23 02:20:52 -0300

    ajuste boton

---
+++ assets/CButton-4bbc2b87.js
@@ -0,0 +1,1 @@
+import{o as n,c as s,t as i}from"./index-dd4685c7.js";const a=["disabled"],d={__name:"CButton",props:{title:{type:String,required:!0},disabled:{type:Boolean,default:!1}},emits:["executeAction"],setup(t,{emit:o}){return(l,e)=>(n(),s("button",{class:"c-button",onClick:e[0]||(e[0]=c=>o("executeAction")),disabled:t.disabled},i(t.title),9,a))}};export{d as _};
