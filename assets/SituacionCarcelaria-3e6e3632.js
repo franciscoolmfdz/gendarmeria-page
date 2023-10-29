2900fdc076ad556aa0361172982b1a40ea8a5a08
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-10-29 17:09:20 -0300

    mejora registro despliegue filtros y archivo situación carcelaria

---
+++ assets/SituacionCarcelaria-3e6e3632.js
@@ -0,0 +1,1 @@
+import{g as e,o as t,f as _,j as c,_ as i,m as r}from"./index-01c65d82.js";import{_ as s}from"./CHeaderBar-27cb1ce3.js";const m={__name:"SituacionCarcelaria",setup(l){const a=e("Estado de la situación carcelaria");return(n,o)=>(t(),_(i,null,{default:c(()=>[r(s,{title:a.value},null,8,["title"])]),_:1}))}};export{m as default};
