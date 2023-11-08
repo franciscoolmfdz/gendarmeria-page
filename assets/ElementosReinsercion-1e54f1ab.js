5de4becfe54394d0383e55fe8bf1f68ce07c3cb1
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-11-07 23:27:15 -0300

    se agregan mejoras estilo css y textos

---
+++ assets/ElementosReinsercion-1e54f1ab.js
@@ -0,0 +1,1 @@
+import{g as t,o as n,f as s,j as _,_ as a,m as o}from"./index-cac83eb8.js";import{_ as l}from"./CHeaderBar-ece7db59.js";const f={__name:"ElementosReinsercion",setup(r){const e=t("Elementos de reinserción");return(c,i)=>(n(),s(a,null,{default:_(()=>[o(l,{title:e.value},null,8,["title"])]),_:1}))}};export{f as default};
