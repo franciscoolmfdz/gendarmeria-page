fe69f7bf7a7abe41e57bfeb8fe0c22457299bdd6
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-11-10 01:22:42 -0300

    se agregan mejoras estilo trayectoria

---
+++ assets/GestionCarcel-d6c39b04.js
@@ -0,0 +1,1 @@
+import{g as t,o as a,f as _,j as s,_ as l,m as c}from"./index-35ecef89.js";import{_ as n}from"./CHeaderBar-260743b4.js";const m={__name:"GestionCarcel",setup(o){const e=t("Gestión de la carcel");return(r,i)=>(a(),_(l,null,{default:s(()=>[c(n,{title:e.value},null,8,["title"])]),_:1}))}};export{m as default};
