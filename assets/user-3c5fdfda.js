da19962a921ce2681d6e80820cd1ba94256107b4
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-11-12 21:16:47 -0300

    mejoras detalles gráficos

---
+++ assets/user-3c5fdfda.js
@@ -0,0 +1,1 @@
+import{q as t}from"./index-0a5b9537.js";const r="/users/validate/_authenticate",e="/users/users",n=async(s,a)=>await t(r,"PATCH",s,a),c=async(s,a)=>await t(e,"POST",s,a);export{c as a,n as p};
