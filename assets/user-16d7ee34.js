246d33586beb614883d45359611da1aedccb6029
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-11-21 08:40:15 -0300

    cambios y mejoras despliegue y marco legal con nuevos camps

---
+++ assets/user-16d7ee34.js
@@ -0,0 +1,1 @@
+import{q as t}from"./index-cf584654.js";const r="/users/validate/_authenticate",e="/users/users",n=async(s,a)=>await t(r,"PATCH",s,a),c=async(s,a)=>await t(e,"POST",s,a);export{c as a,n as p};
