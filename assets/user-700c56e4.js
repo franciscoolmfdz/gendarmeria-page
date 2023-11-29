94851d704030869c6f66cb61389237c64c9bbc2e
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-11-29 15:59:18 -0300

    fix carga marco legal

---
+++ assets/user-700c56e4.js
@@ -0,0 +1,1 @@
+import{y as t}from"./index-27498bb2.js";const r="/users/validate/_authenticate",e="/users/users",n=async(s,a)=>await t(r,"PATCH",s,a),c=async(s,a)=>await t(e,"POST",s,a);export{c as a,n as p};
