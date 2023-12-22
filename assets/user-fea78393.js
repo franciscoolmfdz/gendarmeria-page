2a413a046936518d354cc16aa8c906f14a6acc8d
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-12-22 00:42:51 -0300

    coreccions condenados

---
+++ assets/user-fea78393.js
@@ -0,0 +1,1 @@
+import{y as t}from"./index-67eed0ab.js";const r="/users/validate/_authenticate",e="/users/users",n=async(s,a)=>await t(r,"PATCH",s,a),c=async(s,a)=>await t(e,"POST",s,a);export{c as a,n as p};
