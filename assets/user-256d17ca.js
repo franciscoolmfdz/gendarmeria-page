aa1696444912be2949691a71aa5789918b19df6e
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-12-23 16:35:36 -0300

    mejoras detalles secciones reinsercion y situacion carcelaria p1

---
+++ assets/user-256d17ca.js
@@ -0,0 +1,1 @@
+import{y as t}from"./index-846b6a5a.js";const r="/users/validate/_authenticate",e="/users/users",n=async(s,a)=>await t(r,"PATCH",s,a),c=async(s,a)=>await t(e,"POST",s,a);export{c as a,n as p};
