4f1799324857a809967a498f3a7be480f48d3835
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-12-27 17:30:37 -0300

    mejoras css y resumenes

---
+++ assets/user-2b6efb49.js
@@ -0,0 +1,1 @@
+import{z as t}from"./index-1c9b6d67.js";const r="/users/validate/_authenticate",e="/users/users",n=async(s,a)=>await t(r,"PATCH",s,a),c=async(s,a)=>await t(e,"POST",s,a);export{c as a,n as p};
