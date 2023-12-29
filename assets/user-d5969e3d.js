0044fc9a06f5cb635fbd01ef308d3acff303d45a
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-12-29 00:08:47 -0300

    ajustes titulos y detalles

---
+++ assets/user-d5969e3d.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index-ff297cff.js";const r="/users/validate/_authenticate",e="/users/users",n=async(s,a)=>await t(r,"PATCH",s,a),c=async(s,a)=>await t(e,"POST",s,a);export{c as a,n as p};
