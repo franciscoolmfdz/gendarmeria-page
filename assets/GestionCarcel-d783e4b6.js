b688c9e6ee498f72de8277105e7f8f21c3adf670
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-12-29 01:52:13 -0300

    fix detalle situacion carcelaria

---
+++ assets/GestionCarcel-d783e4b6.js
@@ -0,0 +1,1 @@
+import{g as t,o as a,f as _,j as s,_ as l,m as c}from"./index-252043c9.js";import{_ as n}from"./CHeaderBar-59194ce0.js";const m={__name:"GestionCarcel",setup(o){const e=t("Gestión de la carcel");return(r,i)=>(a(),_(l,null,{default:s(()=>[c(n,{title:e.value},null,8,["title"])]),_:1}))}};export{m as default};
