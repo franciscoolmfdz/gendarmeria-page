d1a50d43d35644800f3f92f83142a28b3e44910a
francisco.olmedo <francisco.olmfdz@gmail.com>
2023-12-29 01:40:19 -0300

    correcciones css

---
+++ assets/user-2d8dee27.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index-2e2a9dba.js";const r="/users/validate/_authenticate",e="/users/users",n=async(s,a)=>await t(r,"PATCH",s,a),c=async(s,a)=>await t(e,"POST",s,a);export{c as a,n as p};
