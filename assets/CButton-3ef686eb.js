0a178a6c0e2e89c105a2e5b1031fd215d4355a08
francisco.olmedo <francisco.olmfdz@gmail.com>
2024-01-06 14:21:34 -0300

    mejoras finales

---
+++ assets/CButton-3ef686eb.js
@@ -0,0 +1,1 @@
+import{o as n,c as s,t as i}from"./index-26d193c9.js";const a=["disabled"],d={__name:"CButton",props:{title:{type:String,required:!0},disabled:{type:Boolean,default:!1}},emits:["executeAction"],setup(t,{emit:o}){return(l,e)=>(n(),s("button",{class:"c-button",onClick:e[0]||(e[0]=c=>o("executeAction")),disabled:t.disabled},i(t.title),9,a))}};export{d as _};
