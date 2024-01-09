8a45fa663b21296d9819a5af2a22b8a8a3b685cd
francisco.olmedo <francisco.olmfdz@gmail.com>
2024-01-09 18:42:25 -0300

    se agrega fecha en marco legal

---
+++ assets/user-c8d66271.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index-9fb49b6f.js";const r="/users/validate/_authenticate",e="/users/users",n=async(s,a)=>await t(r,"PATCH",s,a),c=async(s,a)=>await t(e,"POST",s,a);export{c as a,n as p};
