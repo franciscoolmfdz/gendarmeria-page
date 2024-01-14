914f6fda2accd1515b76ad35c0ce11216959f319
francisco.olmedo <francisco.olmfdz@gmail.com>
2024-01-14 11:10:15 -0300

    correccion detalles y filtros

---
+++ assets/GestionCarcel-78acc330.js
@@ -0,0 +1,1 @@
+import{g as t,o as a,f as _,j as s,_ as l,m as c}from"./index-f62bec79.js";import{_ as n}from"./CHeaderBar-768ede49.js";const m={__name:"GestionCarcel",setup(o){const e=t("Gestión de la carcel");return(r,i)=>(a(),_(l,null,{default:s(()=>[c(n,{title:e.value},null,8,["title"])]),_:1}))}};export{m as default};
